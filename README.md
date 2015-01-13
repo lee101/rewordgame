46bcbe9052140600f44da16586984d4ed8e269e0
Lee Penkman <leepenkman@gmail.com>
2015-01-14 09:26:08 +1300

    Remove references to wordsmashing

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 mmochess
 ============
 
-WordSmashing.com an online word search puzzle game and app
+BigMultiplayerChess.com an online word search puzzle game and app
 
 Big Multiplayer Chess uses Twitter Bootstrap, JQuery and Python running on the Google app engine.
 
-dependencies are managed through pip     ```pip install -r requirements.txt```
 
 
--- main.py
+++ main.py
@@ -37,6 +37,8 @@ class BaseHandler(webapp2.RequestHandler):
             # 'glogin_url': users.create_login_url(self.request.uri),
             # 'glogout_url': users.create_logout_url(self.request.uri),
             'url': self.request.uri,
+            'host': self.request.host,
+            'host_url': self.request.host_url,
             'path': self.request.path,
             'urlencode': urllib.quote_plus,
             # 'num_levels': len(LEVELS)
@@ -124,61 +126,16 @@ class GamesHandler(BaseHandler):
         self.render('templates/index.jinja2', {'noads': True})
 
 
-class LearnEnglishHandler(BaseHandler):
-    def get(self):
-        noads = self.request.get('noads', False)
-        self.render('templates/learn-english.jinja2', {
-            'noads': noads,
-            "json": json,
-            'LEARN_ENGLISH_LEVELS': ((key, level) for key, level in fixtures.LEARN_ENGLISH_LEVELS.iteritems())
-        })
-
-
-class EnglishLevelHandler(BaseHandler):
-    def get(self, urlkey):
-        noads = self.request.get('noads', False)
-        if urlkey == 'undefined':
-            logging.error('learn english key is undefined??')
-            return self.redirect('/learn-english/girls-names')
-        self.render('templates/learn-english-level.jinja2', {
-            'noads': noads,
-            'level': fixtures.LEARN_ENGLISH_LEVELS[urlkey],
-            'urlkey': urlkey,
-        })
-
-
 class CampaignHandler(BaseHandler):
     def get(self):
         noads = self.request.get('noads', False)
         self.render('templates/campaign.jinja2', {'noads': noads})
 
 
-class BuyHandler(BaseHandler):
-    def get(self):
-        # paymentAmount = "3.99"
-        # CURRENCYCODE = "USD"
-        # RETURNURL = "https://wordsmashing.appspot.com/buy"
-        # CANCELURL = "https://wordsmashing.appspot.com/buy"
-
-        self.render('templates/buy.jinja2')
-
-
-class LevelHandler(BaseHandler):
-    def get(self, level):
-        level_num = int(level)
-        noads = self.request.get('noads', False)
-        self.render('templates/level.jinja2', {'noads': noads, 'level_num': level_num, 'level': fixtures.LEVELS[level_num - 1]})
-
-
-class SitemapHandler(webapp2.RequestHandler):
+class SitemapHandler(BaseHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/xml'
-        template_values = {
-            'learnenglishlevels': fixtures.LEARN_ENGLISH_LEVELS,
-        }
-        template = JINJA_ENVIRONMENT.get_template('sitemap.xml')
-        self.response.write(template.render(template_values))
-
+        self.render('sitemap.xml')
 
 class SlashMurdererApp(webapp2.RequestHandler):
     def get(self, url):
@@ -200,8 +157,6 @@ app = ndb.toplevel(webapp2.WSGIApplication([
                                                ('/multiplayer', FriendsHandler),
                                                ('/games-multiplayer', GameMultiplayerHandler),
                                                ('/games', GamesHandler),
-                                               ('/learn-english', LearnEnglishHandler),
-                                               ('/learn-english/(.*)', EnglishLevelHandler),
 
                                                # need js rendering
                                                ('/play', CampaignHandler),
@@ -210,7 +165,6 @@ app = ndb.toplevel(webapp2.WSGIApplication([
 
                                                (r'/tests', TestsHandler),
 
-                                               ('/buy', BuyHandler),
                                                ('/sitemap', SitemapHandler),
 
                                            ] + gameon.routes, debug=ws.debug, config=config))
